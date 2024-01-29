3ac663583c1bc20786816e74a667868fe0e15540
emphurial <jesse.williams1999@hotmail.com>
2024-01-29 13:39:17 +1030

    Name and class inputs finished

--- graphics.py
+++ graphics.py
@@ -6,7 +6,6 @@ class Window():
         self.window = Tk()
         self.window.title('Game Window')
         self.window.geometry('500x500')
-        
         #game introduction
         self._menu_header("Welcome to the World \nof \nINSERT NAME HERE")
         self._menu_button(self.window, "Start", 18, 0.5, self._start_menu)
@@ -41,23 +40,41 @@ class Window():
         self._menu_button(self.window, 'Load Game', 18, 0.6)
         self._menu_button(self.window, 'Back', 18, 0.9, self._back_button)
 
-    def _new_game(self):
+    def _new_game(self, no_name = 0):
+        Player()
         self._new_window('New Game')
+        if no_name == 1:
+            invalid = Label(self.window, text = 'Please enter a name.', fg='red', font=('times new roman', 20))
+            invalid.place(relx=0.5, rely=0.3, anchor=CENTER)
+        self._menu_header("What is your name?")
+        name_entry = Entry(self.window)
+        name_entry.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self._menu_button(self.window, 'Confirm', 18, 0.9, lambda: self._store_name(name_entry.get()))
+
+    def _store_name(self, name_entry):
+        if len(name_entry) == 0:
+            self._new_game(1)
+        else:  
+            Player._player_name = name_entry
+            self._choose_class()
+        
+    def _choose_class(self):
+        self._new_window('Choose Class')
         self._menu_header('Choose your class.')
         self._menu_button(self.window, 'Warrior', 18, 0.5, self._warrior_chosen)
         self._menu_button(self.window, 'Wizard', 18, 0.6, self._wizard_chosen)
         self._menu_button(self.window, 'Back', 18, 0.9, self._back_button)
 
     def _warrior_chosen(self):
-        Player._warrior()
+        Player._warrior(Player)
         self._game_start()
 
     def _wizard_chosen(self):
-        Player._wizard()
+        Player._wizard(Player)
         self._game_start()
 
     def _game_start(self):
-        self._new_window('The Journey Begins')
+        self._new_window('The Journey Begins', '700x700')
         self._menu_button(self.window, 'Next', 18, 0.9)
 
     def _back_button(self):
--- player.py
+++ player.py
@@ -1,30 +1,33 @@
+from items import *
+
 class Player():
     def __init__(self):
+        self._player_name = None
         self._player_class = None
         self._player_level = 1
         self._player_xp = 0
+        self._player_gold = 0
         self._player_health = 100
         self._player_attack = 10
         self._player_mana = 10
 
     def _warrior(self):
-        self._player_class = 0
+        self._player_class = 'Warrior'
         self._player_attack = 20
 
     def _wizard(self):
+        self._player_class = 'Wizard'
         self._player_health = 80
         self._player_mana = 50
 
     def _level_up(self):
-        levels = {}
-        for i in range(10):
-            levels[i] = i * 10
+        levels = [0, 10, 40, 100, 180, 280, 400, 540, 700, 880]
         if self._player_xp > levels[self._player_level + 1]:
             self._player_level += 1
-            if self._player_class == 0:
+            if self._player_class == 'Warrior':
                 self._player_health += 40
                 self._player_attack += 5
-            if self._player_class == 1:
+            if self._player_class == 'Wizard':
                 self._player_health += 25
                 self._player_mana += 20
 
